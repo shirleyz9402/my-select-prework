06d35322bb12fc02e7b95ed878d83ec049e9ccd2
Shirley Zhang <shirleyz9402@yahoo.com>
2018-01-23 21:25:03 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,11 @@
 def my_select(array) 
 i = 0
 arr = []
-while i < array.size
-  if yield(array[i]) == true
-  arr.push(array[i])
-end
-  i+=1
-end
-arr
+  while i < array.size
+    if yield(array[i]) == true
+    arr.push(array[i])
+  end
+    i+=1
+  end
+  arr
 end
